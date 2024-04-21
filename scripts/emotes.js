61f5ea376cb52ae9a9500332bcdf634cb24f2792
Brandin Canterbury <BCanterbury@firstkeyhomes.com>
2024-04-21 04:15:21 -0400

    Initial Commit

---
+++ scripts/emotes.js
@@ -0,0 +1,16 @@
+﻿Hooks.on("preCreateChatMessage", async (action) => {
+    console.log(action);
+    // let rollTable = findRollTableForAction(action);
+    // if (rollTable) {
+    //     let result = await rollTable.roll();
+    //     displayEmote(result.text, action.token);
+    // }
+});
+
+function findRollTableForAction(action) {
+    // Retrieve the linked roll table based on the action
+}
+
+function displayEmote(text, tokenId) {
+    // Existing code to display the emote
+}
